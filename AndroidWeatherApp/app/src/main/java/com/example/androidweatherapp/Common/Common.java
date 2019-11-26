27df626c64d2e5579820a300c4b3f6924dae1f7d
riddar13 <40752707+riddar13@users.noreply.github.com>
2019-11-27 01:07:09 +0600

    add common, model, retrofit

---
+++ AndroidWeatherApp/app/src/main/java/com/example/androidweatherapp/Common/Common.java
@@ -0,0 +1,6 @@
+package com.example.androidweatherapp.Common;
+
+public class Common {
+
+    public static final String APP_ID = "d93688a4d867919980b285aac46e1abd";
+}
---
+++ AndroidWeatherApp/app/src/main/java/com/example/androidweatherapp/Model/WeatherResult.java
@@ -0,0 +1,4 @@
+package com.example.androidweatherapp.Model;
+
+public class WeatherResult {
+}
