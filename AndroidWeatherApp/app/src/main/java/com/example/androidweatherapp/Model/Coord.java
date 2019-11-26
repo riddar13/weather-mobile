8e8c3831e149a6cc9c8d864b93faef11493de588
riddar13 <40752707+riddar13@users.noreply.github.com>
2019-11-27 02:13:15 +0600

    fix coord

--- AndroidWeatherApp/app/src/main/java/com/example/androidweatherapp/Model/Coord.java
+++ AndroidWeatherApp/app/src/main/java/com/example/androidweatherapp/Model/Coord.java
@@ -22,4 +22,9 @@ public class Coord {
     public void setLat(double lat) {
         this.lat = lat;
     }
+
+    @Override
+    public String toString(){
+        return new StringBuilder("[").append(this.lat).append(',').append(this.lon).append(']').toString();
+    }
 }
